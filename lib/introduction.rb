d8af27532a2f5d1b2d240fcb798cf23d7f755995
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-09 20:12:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,12 @@ end
 introduction("Josh")
 introduction("Dan")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Josh", "React")
 
 
 
